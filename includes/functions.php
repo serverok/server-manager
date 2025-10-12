9de27e4a4a6c77b17c2aca8d8cd09eb2428e04e6
Yujin Boby <serverok.in@gmail.com>
2025-10-12 14:09:32 +0530

    correct log file permission

--- includes/functions.php
+++ includes/functions.php
@@ -4,7 +4,6 @@ function sokLog($message, $display = false) {
     $log_file = '/var/log/server-manager.log';
     $formatted_message = date('[Y-m-d H:i:s] ') . $message . PHP_EOL;
     file_put_contents($log_file, $formatted_message, FILE_APPEND);
-
     if ($display) {
         echo $message . PHP_EOL;
     }
